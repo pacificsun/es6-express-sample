32cf21e633699da35a17871faf1217141f85f76c
Suraj Thapa <stsurajst@gmail.com>
2020-09-25 09:04:17 +0545

    dependencies added

---
+++ dist-server/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+var PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+  return console.log("app is running on PORT ".concat(PORT));
+});
